c5c0dc0ae94002a88f9316259a14d58dc316d5be
Katharine Chen & Scott Dudley <student@epicodus.com>
2014-07-23 13:58:54 -0700

    Spec Saved

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -64,16 +64,3 @@ describe("roman_numerals") do
   end
 
 end
-
-
-
-
-# The most basic rule is that you add the value of all the symbols: so II is 2, LXVI is 66, etc.
-
-# The exception is that you can't have more than three of the same character is a row: instead, you switch to subtraction. So instead of writing IIII for 4, you write IV (for 5 minus 1); and instead of writing LXXXX for 90, you write XC.
-
-# You also have to separate ones, tens, hundreds, and thousands. In other words, 99 is XCIX, not IC.
-
-# You can't count higher than 3,999 in Roman numerals.
-
-# Write a method to convert numbers into roman numerals. Let BDD be your guiding light.
